26f4dc84ad46a7646e56b007407169ad35e58f61
Wouter Visser <whtspc@gmail.com>
2022-11-25 12:56:42 +0100

    datatable with data

--- wc2022/force-app/main/default/lwc/worldCup2022/worldCup2022.js
+++ wc2022/force-app/main/default/lwc/worldCup2022/worldCup2022.js
@@ -1,3 +1,39 @@
 import { LightningElement } from 'lwc';
 
-export default class WorldCup2022 extends LightningElement {}
+const columns = [
+    { label: 'Venue', fieldName: 'venue' , type: 'text'},
+    { label: 'Home Team', fieldName: 'home_team', type: 'text'},
+    { label: 'Away Team', fieldName: 'away_team', type: 'text'},
+    { label: 'Kick Off', fieldName: 'kick_off', type: 'text' }
+];
+
+export default class WorldCup2022 extends LightningElement {
+
+    data = [];
+    columns = columns;
+
+
+    connectedCallback(){
+
+        this.getToday();
+
+  
+    }
+
+    async getToday(){
+        const response = await fetch('https://worldcupjson.net/matches/today');
+        const todayJson = await response.json();
+        for (const entry of todayJson){
+            let match = {};
+            match.venue = entry.venue;
+            match.home_team = entry.home_team.name;
+            match.away_team = entry.away_team.name;
+            match.kick_off = entry.datetime;
+            this.data.push(match);
+
+        }
+        
+        this.data = [... this.data]
+
+    }
+}
