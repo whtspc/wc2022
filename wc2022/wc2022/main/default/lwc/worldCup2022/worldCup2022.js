b360f008fddc9ca68809255f5e34a875599021fe
JowinWaaijer <jowin.waaijer@brightfox.eu>
2022-11-26 00:08:45 +0100

    Created base component for upcoming WC2022 matches.

---
+++ wc2022/wc2022/main/default/lwc/worldCup2022/worldCup2022.js
@@ -0,0 +1,95 @@
+import {LightningElement, track} from 'lwc';
+import WC_QATAR_LOGO from '@salesforce/resourceUrl/wc_qatar_logo';
+import FlagService from "c/flagService";
+
+export default class WorldCup2022 extends LightningElement {
+
+    @track data = [];
+    @track currentMatch;
+
+    columns = columns;
+    logo = WC_QATAR_LOGO;
+    @track flags = {
+        home: null,
+        away: null
+    }
+    flagService;
+
+    async connectedCallback() {
+
+        this.flagService = new FlagService();
+        this.flags.home = this.flagService.getFlag('NL');
+        this.flags.away = this.flagService.getFlag('EC');
+
+        this.currentMatch = await this.getCurrent();
+        this.data = await this.getToday();
+
+
+    }
+
+    async getCurrent() {
+        const response = await fetch('https://worldcupjson.net/matches/current');
+        const currentJSON = await response.json();
+
+        if(currentJSON.length === 0)
+            return null;
+        const entry = currentJSON[0];
+        let match = {};
+        const homeCode = this.flagService.getCode(entry.home_team.country);
+        const awayCode = this.flagService.getCode(entry.away_team.country);
+
+        if(!homeCode.flag) {
+            homeCode.flag = this.flagService.getFlag(homeCode.ISO);
+        }
+        if(!awayCode.flag) {
+            awayCode.flag = this.flagService.getFlag(awayCode.ISO);
+        }
+
+        match.venue = entry.venue;
+        match.home_team = entry.home_team.country;
+        match.home_team_flag = homeCode.flag;
+        match.away_team = entry.away_team.country;
+        match.away_team_flag = awayCode.flag;
+        match.kick_off = entry.datetime;
+        match.completed = entry.status === 'completed';
+        match.in_progress = entry.status !== 'completed' && entry.status !== 'future_scheduled';
+        match.score = `${entry.home_team.goals} - ${entry.away_team.goals}`
+        return match;
+    }
+
+    async getToday() {
+        const response = await fetch('https://worldcupjson.net/matches/today');
+        const todayJson = await response.json();
+        const data = [];
+        todayJson.forEach(entry => {
+            console.log(entry);
+
+            const homeCode = this.flagService.getCode(entry.home_team.country);
+            const awayCode = this.flagService.getCode(entry.away_team.country);
+
+            if(!homeCode.flag) {
+               homeCode.flag = this.flagService.getFlag(homeCode.ISO);
+            }
+            if(!awayCode.flag) {
+                awayCode.flag = this.flagService.getFlag(awayCode.ISO);
+            }
+
+            let match = {};
+            match.venue = entry.venue;
+            match.home_team = entry.home_team.name;
+            match.home_team_flag = homeCode.flag;
+            match.away_team = entry.away_team.name;
+            match.away_team_flag = awayCode.flag;
+            match.kick_off = entry.datetime;
+            match.completed = entry.status === 'completed';
+            match.in_progress = entry.status !== 'completed' && entry.status !== 'future_scheduled';
+            match.score = `${entry.home_team.goals} - ${entry.away_team.goals}`
+            data.push(match);
+        });
+
+        return data;
+
+    }
+
+
+}
