4f60784e1a0ee0ce7fc3f3839d80b7bec477dac8
JowinWaaijer <jowinw@gmail.com>
2022-11-28 07:50:25 +0100

    Created a new release (#5)
    * Created base component for upcoming WC2022 matches.
    
    * updated gitignore
    
    * Fixed small error
    
    * Removed static flag loading
    
    * Removed static flag loading
    
    * Rmoved project files
    
    * Added a refresh timer / poller
    Added first set of statistics.
    Some styling
    
    * Update sfdx-project.json
    
    Updated package versions
    
    * End of day commit.
    
    * Fixed spanish flags.
    Added line-ups to current match
    
    * Released a new version
    
    * Added transparant background logo and a background with gradient instead of in the PNG to fix the header height.
    
    Co-authored-by: JowinWaaijer <jowin.waaijer@brightfox.eu>

--- wc2022/wc2022/main/default/lwc/flagService/flagService.js
+++ wc2022/wc2022/main/default/lwc/flagService/flagService.js
@@ -81,7 +81,7 @@ export default class FlagService {
             ISO: 'RS'
         },
         ESP: {
-            ISO: 'SP'
+            ISO: 'ES'
         },
         SUI: {
             ISO: 'CH'
